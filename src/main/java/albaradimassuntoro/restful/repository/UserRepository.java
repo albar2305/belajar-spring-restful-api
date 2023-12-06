31b3765758116f6861fbadc6879231296efd53c2
Albar Adimas Suntoro <albar230505@gmail.com>
2023-12-07 06:24:43 +0700

    add: register and login user api

---
+++ src/main/java/albaradimassuntoro/restful/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package albaradimassuntoro.restful.repository;
+
+import albaradimassuntoro.restful.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
